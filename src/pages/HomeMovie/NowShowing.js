b963acdc4477b67af64e261f6129e2048f036089
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-04-09 08:46:02 +0700

    Now Showing

--- src/pages/HomeMovie/NowShowing.js
+++ src/pages/HomeMovie/NowShowing.js
@@ -1,5 +1,35 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NEMO } from "../../utils/config";
+import { getMoviesListAction } from "../../redux/actions";
+import { Container } from "../../styles/Container";
+import { Heading } from "../../styles/Heading";
+import { MovieGridV1, MovieGridV2 } from "../../components/MovieCard/MovieGrid";
+import { MovieCardV1 } from "../../components/MovieCard/V1/MovieCardV1";
+import { MovieCardV2 } from "../../components/MovieCard/V2/MovieCardV2";
 export default function NowShowing() {
-  return <div>NowShowing</div>;
+  const movies = useSelector((state) => state.MovieReducer.nowShowing);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMoviesListAction());
+    document.title = `Now Showing Movies - ${NEMO}`;
+  }, [dispatch]);
+
+  return (
+    <Container>
+      <Heading>Now Showing</Heading>
+      <MovieGridV1>
+        {movies.map((movie, index) => (
+          <MovieCardV1 movie={movie} key={index} />
+        ))}
+      </MovieGridV1>
+
+      <MovieGridV2>
+        {movies.map((movie, index) => (
+          <MovieCardV2 movie={movie} key={index} />
+        ))}
+      </MovieGridV2>
+    </Container>
+  );
 }
