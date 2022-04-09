ee8c814c6af8cdac00cfc4a9c393f59f44e4eab4
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-04-09 08:45:50 +0700

    Coming Soon

--- src/pages/HomeMovie/ComingSoon.js
+++ src/pages/HomeMovie/ComingSoon.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NEMO } from "../../utils/config";
+import { getMoviesListAction } from "../../redux/actions";
+import { Container } from "../../styles/Container";
+import { Heading } from "../../styles/Heading";
+import { MovieGridV1, MovieGridV2 } from "../../components/MovieCard/MovieGrid";
+import { MovieCardV1 } from "../../components/MovieCard/V1/MovieCardV1";
+import { MovieCardV2 } from "../../components/MovieCard/V2/MovieCardV2";
 
 export default function ComingSoon() {
+  const movies = useSelector((state) => state.MovieReducer.comingSoon);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMoviesListAction());
+    document.title = `Coming Soon Movies - ${NEMO}`;
+  }, [dispatch]);
+
   return (
-    <div>ComingSoon</div>
-  )
+    <Container>
+      <Heading>Coming Soon</Heading>
+      <MovieGridV1>
+        {movies.map((movie, index) => (
+          <MovieCardV1 movie={movie} key={index} />
+        ))}
+      </MovieGridV1>
+
+      <MovieGridV2>
+        {movies.map((movie, index) => (
+          <MovieCardV2 movie={movie} key={index} />
+        ))}
+      </MovieGridV2>
+    </Container>
+  );
 }
