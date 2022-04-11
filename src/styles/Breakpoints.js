35da0170dbd1a13231c8b09e70ae35701c3f8d54
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-04-11 12:38:38 +0700

    Breakpoints

---
+++ src/styles/Breakpoints.js
@@ -0,0 +1,13 @@
+const size = {
+  sm: "600px",
+  md: "768px",
+  lg: "1050px",
+  xl: "1440px",
+};
+
+export const Breakpoints = {
+  sm: `@media only screen and (max-width: ${size.sm})`,
+  md: `@media only screen and (max-width: ${size.md})`,
+  lg: `@media only screen and (max-width: ${size.lg})`,
+  xl: `@media only screen and (max-width: ${size.xl})`,
+};
