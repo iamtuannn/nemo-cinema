bb9a1d34d1d8379fe4cbd6cbab4bbabebc4851d8
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-04-11 13:31:44 +0700

    Pagination

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { FaChevronCircleRight, FaChevronCircleLeft } from "react-icons/fa";
+
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Pagination({ page, totalPages }) {
+  const navigate = useNavigate();
+
+  return (
+    <S.Box>
+      <p>
+        Page <span style={{ color: "var(--color-red)" }}>{page}</span>
+        {` of ${totalPages}`}
+      </p>
+      <S.Flex>
+        {page > 1 ? (
+          <S.Button onClick={() => navigate(`/actor/${page - 1}`)}>
+            <FaChevronCircleLeft />
+          </S.Button>
+        ) : null}
+        {page < totalPages ? (
+          <S.Button onClick={() => navigate(`/actor/${page + 1}`)}>
+            <FaChevronCircleRight />
+          </S.Button>
+        ) : null}
+      </S.Flex>
+    </S.Box>
+  );
+}
+
+const S = {
+  Box: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 1.5rem 0;
+    font-family: "Khand", sans-serif;
+  `,
+  Flex: styled.div`
+    display: flex;
+  `,
+  Button: styled.div`
+    background: transparent;
+    border: 1px solid transparent;
+    cursor: pointer;
+
+    && svg {
+      height: 2rem;
+      width: 2rem;
+      margin-left: 0.5rem;
+    }
+  `,
+};
