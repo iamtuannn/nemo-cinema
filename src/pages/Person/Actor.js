ec2f5ee662754f8d678702f02e1016362b7239a5
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-04-11 13:34:09 +0700

    Actor

--- src/pages/Person/Actor.js
+++ src/pages/Person/Actor.js
@@ -1,7 +1,124 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Breakpoints } from "../../styles/Breakpoints";
+import male from "../../images/people-male.svg";
+import female from "../../images/people-female.svg";
+import Loading from "../../components/Loading/Loading";
+import { BASE_API_PERSON_URL } from "../../utils/config";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getActorList } from "../../redux/actions";
+import { Container } from "../../styles/Container";
+import { Heading } from "../../styles/Heading";
+import Pagination from "../../components/Pagination/Pagination";
+import LazyLoad from "react-lazyload";
 
 export default function Actor() {
+  const { actor, totalPages } = useSelector((state) => state.PeopleReducer);
+  const { isLoading } = useSelector((state) => state.LoadingReducer);
+  const params = useParams();
+  const page = parseInt(params.id) || 1;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getActorList(page));
+    window.scroll(0, 0);
+  }, [dispatch, page]);
+
   return (
-    <div>Actor</div>
-  )
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Container>
+          <Heading>Popular People</Heading>
+          <S.Grid>
+            {actor.map((person, index) => (
+              <Link
+                to={`/person/${person.name
+                  .toLowerCase()
+                  .split(" ")
+                  .join("-")}-${person.id}`}
+                target="_parent"
+                key={index}
+              >
+                <LazyLoad height={400}>
+                  <S.Card>
+                    <S.Image
+                      src={
+                        person.profile_path === null && person.gender === 1
+                          ? female
+                          : person.profile_path === null
+                          ? male
+                          : BASE_API_PERSON_URL + person.profile_path
+                      }
+                      alt={person.name}
+                    ></S.Image>
+                    <S.Content>
+                      <S.Name>{person.name}</S.Name>
+                    </S.Content>
+                  </S.Card>
+                </LazyLoad>
+              </Link>
+            ))}
+          </S.Grid>
+          <Pagination page={page} totalPages={totalPages} />
+        </Container>
+      )}
+    </>
+  );
 }
+
+const S = {
+  Grid: styled.div`
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(5, minmax(0, 1fr));
+
+    ${Breakpoints.lg} {
+      grid-template-columns: repeat(4, minmax(0, 1fr));
+    }
+
+    ${Breakpoints.md} {
+      grid-template-columns: repeat(3, minmax(0, 1fr));
+    }
+
+    ${Breakpoints.sm} {
+      grid-template-columns: repeat(2, minmax(0, 1fr));
+    }
+  `,
+  Card: styled.div`
+    box-sizing: 0.5rem;
+    overflow: hidden;
+    position: relative;
+    animation: fade-in 1s ease-in-out 0s both;
+  `,
+  Image: styled.img`
+    max-width: 100%;
+    background-color: var(--color-secondary);
+  `,
+  Content: styled.div`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    min-height: 3rem;
+    background: rgba(66, 63, 87, 0.7);
+    padding: 0.25rem 0.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  Name: styled.span`
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    text-transform: uppercase;
+    font-weight: 600;
+    font-family: "Changa", sans-serif;
+    text-align: center;
+    color: #fff;
+  `,
+};
