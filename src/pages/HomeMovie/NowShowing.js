df966115eabcdbd305a2a3350e20e1bd5d50d0d6
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-04-11 12:37:38 +0700

    Update NowShowing.js

--- src/pages/HomeMovie/NowShowing.js
+++ src/pages/HomeMovie/NowShowing.js
@@ -4,11 +4,15 @@ import { NEMO } from "../../utils/config";
 import { getMoviesListAction } from "../../redux/actions";
 import { Container } from "../../styles/Container";
 import { Heading } from "../../styles/Heading";
-import { MovieGridV1, MovieGridV2 } from "../../components/MovieCard/MovieGrid";
+import { MovieGridV1, MovieGridV2 } from "../../components/MovieGrid/MovieGrid";
 import { MovieCardV1 } from "../../components/MovieCard/V1/MovieCardV1";
 import { MovieCardV2 } from "../../components/MovieCard/V2/MovieCardV2";
+import { Skeleton } from "antd";
+import Loading from "../../components/Loading/Loading";
 export default function NowShowing() {
   const movies = useSelector((state) => state.MovieReducer.nowShowing);
+  const { isLoading } = useSelector((state) => state.LoadingReducer);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -18,18 +22,23 @@ export default function NowShowing() {
 
   return (
     <Container>
-      <Heading>Now Showing</Heading>
-      <MovieGridV1>
-        {movies.map((movie, index) => (
-          <MovieCardV1 movie={movie} key={index} />
-        ))}
-      </MovieGridV1>
-
-      <MovieGridV2>
-        {movies.map((movie, index) => (
-          <MovieCardV2 movie={movie} key={index} />
-        ))}
-      </MovieGridV2>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Heading>Now Showing</Heading>
+          <MovieGridV1>
+            {movies.map((movie, index) => (
+              <MovieCardV1 movie={movie} key={index} />
+            ))}
+          </MovieGridV1>
+          <MovieGridV2>
+            {movies.map((movie, index) => (
+              <MovieCardV2 movie={movie} key={index} />
+            ))}
+          </MovieGridV2>
+        </>
+      )}
     </Container>
   );
 }
