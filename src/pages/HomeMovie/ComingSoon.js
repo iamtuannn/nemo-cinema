2510f0909ec897e45c7435ded773b36d7f837b26
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-04-11 12:37:35 +0700

    Update ComingSoon.js

--- src/pages/HomeMovie/ComingSoon.js
+++ src/pages/HomeMovie/ComingSoon.js
@@ -4,12 +4,16 @@ import { NEMO } from "../../utils/config";
 import { getMoviesListAction } from "../../redux/actions";
 import { Container } from "../../styles/Container";
 import { Heading } from "../../styles/Heading";
-import { MovieGridV1, MovieGridV2 } from "../../components/MovieCard/MovieGrid";
+import { MovieGridV1, MovieGridV2 } from "../../components/MovieGrid/MovieGrid";
 import { MovieCardV1 } from "../../components/MovieCard/V1/MovieCardV1";
 import { MovieCardV2 } from "../../components/MovieCard/V2/MovieCardV2";
+import { Skeleton } from "antd";
+import Loading from "../../components/Loading/Loading";
 
 export default function ComingSoon() {
   const movies = useSelector((state) => state.MovieReducer.comingSoon);
+  const { isLoading } = useSelector((state) => state.LoadingReducer);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -19,18 +23,23 @@ export default function ComingSoon() {
 
   return (
     <Container>
-      <Heading>Coming Soon</Heading>
-      <MovieGridV1>
-        {movies.map((movie, index) => (
-          <MovieCardV1 movie={movie} key={index} />
-        ))}
-      </MovieGridV1>
-
-      <MovieGridV2>
-        {movies.map((movie, index) => (
-          <MovieCardV2 movie={movie} key={index} />
-        ))}
-      </MovieGridV2>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Heading>Coming Soon</Heading>
+          <MovieGridV1>
+            {movies.map((movie, index) => (
+              <MovieCardV1 movie={movie} key={index} />
+            ))}
+          </MovieGridV1>
+          <MovieGridV2>
+            {movies.map((movie, index) => (
+              <MovieCardV2 movie={movie} key={index} />
+            ))}
+          </MovieGridV2>
+        </>
+      )}
     </Container>
   );
 }
