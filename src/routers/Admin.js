e8c81cbe2875b356cfd7d37d93c46d8cf078e353
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-04-13 08:26:17 +0700

    routers

--- src/routers/Admin.js
+++ src/routers/Admin.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "../admin/Dashboard/Dashboard";
+import ManageMovie from "../admin/Movies/ManageMovies";
+import AddMovie from "../admin/Movies/AddMovie";
+import EditMovie from "../admin/Movies/EditMovie";
+import AddShowtime from "../admin/Movies/AddShowtime";
+import ManageNews from "../admin/News/ManageNews";
+import AddNews from "../admin/News/AddNews";
+import EditNews from "../admin/News/EditNews";
+import ManageUser from "../admin/Users/ManageUser";
 import NavbarAdmin from "../components/Navbar/NavbarAdmin";
-import Dashboard from "../pages/Dashboard";
-import ManageMovie from "../pages/AdminMovie/ManageMovies";
-import AddMovie from "../pages/AdminMovie/AddMovie";
-import EditMovie from "../pages/AdminMovie/EditMovie";
-import AddShowtime from "../pages/AdminMovie/AddShowtime";
-import ManageNews from "../pages/AdminNews/ManageNews";
-import AddNews from "../pages/AdminNews/AddNews";
-import EditNews from "../pages/AdminNews/EditNews";
-import ManageUser from "../pages/User/ManageUser";
 import { USER_LOGIN } from "../utils/config";
 
 export default function Admin() {
--- src/routers/Home.js
+++ src/routers/Home.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import NavbarHome from "../components/Navbar/NavbarHome";
-import Booking from "../pages/HomeMovie/Booking";
-import Homepage from "../pages/Homepage";
-import Person from "../pages/Person/Person";
-import Actor from "../pages/Person/Actor";
-import MovieDetail from "../pages/HomeMovie/MovieDetail";
-import NowShowing from "../pages/HomeMovie/NowShowing";
-import ComingSoon from "../pages/HomeMovie/ComingSoon";
-import Login from "../pages/User/Login";
-import Register from "../pages/User/Register";
-import LatestNews from "../pages/HomeNews/LatestNews";
-import News from "../pages/HomeNews/News";
-import Profile from "../pages/User/UserProfile";
-import BookingHistory from "../pages/User/BookingHistory";
+import Booking from "../home/Booking/Booking";
+import NowShowing from "../home/Movies/NowShowing";
+import ComingSoon from "../home/Movies/ComingSoon";
+import Detail from "../home/Detail/MovieDetail";
+import LatestNews from "../home/News/LatestNews";
+import News from "../home/News/News";
+import Person from "../home/People/Person";
+import People from "../home/People/People";
+import Login from "../home/User/Login";
+import SignUp from "../home/User/SignUp";
+import Homepage from "../home/Homepage/Homepage";
+import BookingHistory from "../home/User/BookingHistory";
+import UserInfo from "../home/User/UserInfo";
 
 export default function Home() {
   return (
@@ -21,18 +21,19 @@ export default function Home() {
       <NavbarHome />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/movie/:url-:id/" element={<MovieDetail />} />
+        <Route path="/movie/:url-:id/" element={<Detail />} />
         <Route path="/booking/:id" element={<Booking />} />
         <Route path="/login/" element={<Login />} />
         <Route path="/person/:nameUrl-:id/" element={<Person />} />
-        <Route path="/actor/" element={<Actor />} />
+        <Route path="/people/" element={<People />} />
+        <Route path="/people/:id" element={<People />} />
         <Route path="/news/" element={<LatestNews />} />
         <Route path="/news/:url/" element={<News />} />
-        <Route path="/profile/" element={<Profile />} />
+        <Route path="/profile/" element={<UserInfo />} />
         <Route path="/history/" element={<BookingHistory />} />
         <Route path="/now-showing" element={<NowShowing />} />
         <Route path="/coming-soon" element={<ComingSoon />} />
-        <Route path="/register/" element={<Register />} />
+        <Route path="/signup/" element={<SignUp />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
