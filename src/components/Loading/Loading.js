49b4b50ddcf1cdea18527e185fdfd00766cca6f7
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-04-15 11:47:27 +0700

    Update Loading.js

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,64 +1,112 @@
-import React from "react";
 import styled from "styled-components";
-import { keyframes } from "styled-components";
+import { GridCardV1, GridCardV2 } from "../../styles/Styles";
 
-export default function Loading() {
-  return (
-    <S.Box>
-      <S.BoxLoading>
-        <S.Square />
-      </S.BoxLoading>
-    </S.Box>
+export const LoadingCardV1 = () => <V1 />;
+export const LoadingCardV2 = () => <V2 />;
+export const LoadingPageV0 = () => <V0 />;
+export const LoadingPageV1 = (props) => (
+  <GridCardV2 grid={props.grid ? true : false}>
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+    <V1 />
+  </GridCardV2>
+);
+export const LoadingPageV2 = (props) => (
+  <GridCardV1 grid={props.grid ? true : false}>
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+    <V2 />
+  </GridCardV1>
+);
+
+// horizontal rectangle
+const V1 = styled.div`
+  height: 250px;
+  width: 100%;
+  animation: loading 2s ease-in-out infinite;
+  background-image: linear-gradient(
+    90deg,
+    hsla(248, 16%, 19%, 0.3) 25%,
+    hsla(0, 56%, 53%, 0.25) 37%,
+    hsla(248, 16%, 19%, 0.4) 63%
   );
-}
-
-const spin = keyframes`
-    50%,100% {
-        transform: rotate(360deg);
-    }
+  background-size: 400% 100%;
+  margin: 0.5rem 0;
+  border-radius: 4px;
 `;
 
-const fill = keyframes`
-    25%,50% {
-        transform: scaleY(0);
-    }
-    100% {
-        transform: scaleY(1);
-    }
+// vertical rectangle
+const V2 = styled.div`
+  width: 100%;
+  border-radius: 0.5rem;
+  background-size: 400% 100%;
+  animation: loading 2s ease-in-out infinite;
+  background-image: linear-gradient(
+    90deg,
+    hsla(248, 16%, 19%, 0.3) 25%,
+    hsla(0, 56%, 53%, 0.25) 37%,
+    hsla(248, 16%, 19%, 0.4) 63%
+  );
+
+  height: calc(((100vw - 5rem) / 2) * 1.5);
+
+  @media (min-width: 768px) {
+    height: calc(((100vw - 6rem) / 3) * 1.5);
+  }
+  @media (min-width: 1024px) {
+    height: calc(((100vw - 7rem) / 4) * 1.5);
+  }
+
+  @media (min-width: 1440px) {
+    height: calc(((1440px - 8rem) / 5) * 1.5);
+  }
 `;
 
-const S = {
-  Box: styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
-    transform: translateY(-13%);
-  `,
-  BoxLoading: styled.div`
-    transform: translateY(-13%);
-    width: 5em;
-    height: 5em;
-  `,
-  Square: styled.div`
-    position: relative;
-    width: 5em;
-    height: 5em;
-    border: 3px solid rgb(202, 66, 66);
-    overflow: hidden;
-    animation: ${spin} 3s ease infinite;
-
-    ::before {
-      content: "";
-      position: absolute;
-      top: -3px;
-      left: -3px;
-      width: 5em;
-      height: 5em;
-      background-color: rgb(202 66 66 / 80%);
-      transform-origin: center bottom;
-      transform: scaleY(1);
-      animation: ${fill} 3s linear infinite;
-    }
-  `,
-};
+//full page
+const V0 = styled.div`
+  height: 100vh;
+  width: 100%;
+  animation: loading 2s ease-in-out infinite;
+  background-image: linear-gradient(
+    90deg,
+    hsla(248, 16%, 19%, 0.3) 25%,
+    hsla(0, 56%, 53%, 0.25) 37%,
+    hsla(248, 16%, 19%, 0.4) 63%
+  );
+  background-size: 400% 100%;
+  border-radius: 0.5rem;
+`;
